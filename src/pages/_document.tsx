bd51692ba0054c3338580b952932d15e324d8be9
Boli Guan <ifendoe@gmail.com>
2023-04-28 15:27:54 +0800

    Add viewport

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <meta
+          name="viewport"
+          content="width=device-width,height=device-height,inital-scale=1.0,maximum-scale=1.0,user-scalable=no"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
